95862f19090f86095d7928dc4f78704a396407a3
robimalco <roberto.malcotti@datangels.com>
2020-04-14 01:14:53 +0200

    Add file

--- my_experiments/movingSin/ofApp.cpp
+++ my_experiments/movingSin/ofApp.cpp
@@ -1,1 +1,79 @@
+#include "ofApp.h"
 
+//--------------------------------------------------------------
+void ofApp::setup(){
+	ofSetCircleResolution(100);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	ofBackground(0);
+	float time = ofGetElapsedTimef();
+	for (int i = 0; i < 900; i++) {
+		ofSetColor(0, 0, 255);
+		ofDrawCircle(
+			ofGetWidth() / 2 + 100 * sin(i * 0.02 + time),
+			i,
+			50 + 40 * sin(i * 0.004 + time));
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
