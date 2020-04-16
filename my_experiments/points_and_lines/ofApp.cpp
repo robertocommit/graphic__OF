9398c991ec37c19717304bcd71dfc3416a06ae62
robimalco <roberto.malcotti@datangels.com>
2020-04-17 01:24:18 +0200

    Add files

--- my_experiments/points_and_lines/ofApp.cpp
+++ my_experiments/points_and_lines/ofApp.cpp
@@ -1,1 +1,89 @@
+#include "ofApp.h"
 
+//--------------------------------------------------------------
+void ofApp::setup(){
+	ofSetFrameRate(3);
+	ofBackground(0);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	int count = 1;
+	while (count < 10) {
+		std::cout << "count: " << count << endl;
+
+		y = (int) ofRandom(0, ofGetHeight());
+		x = (int) ofRandom(0, ofGetWidth());
+		std::cout << "y: " << y << endl;
+		std::cout << "x: " << x << endl;
+
+		// Line
+		polyline.addVertex(ofPoint(x, y));
+		polyline.draw();
+
+		// Circle
+		ofDrawCircle(x, y, 20);
+
+		count++;
+	}
+	polyline.clear();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+	polyline.clear();
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
