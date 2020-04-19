da881e5956ff920fa09f56cdc5790d00b476b8a4
robimalco <roberto.malcotti@datangels.com>
2020-04-19 20:40:28 +0200

    Add files

--- tutorial/openFrameworksTutorialSeries/038/ofApp.cpp
+++ tutorial/openFrameworksTutorialSeries/038/ofApp.cpp
@@ -1,1 +1,93 @@
+#include "ofApp.h"
 
+//--------------------------------------------------------------
+void ofApp::setup(){
+
+	// Allocate 2D array to manage colors
+	colors = new int*[AMOUNT];  
+	for (int i = 0; i < AMOUNT; i++) {  
+  		colors[i] = new int[3];
+  	}
+
+	ofBackground(0);
+	speedx.resize(AMOUNT);
+	speedy.resize(AMOUNT);
+	for (int i = 0; i< AMOUNT; i++) {
+		speedx[i] = ofRandom(0.0001, 0.99);
+		speedy[i] = ofRandom(0.0001, 0.90);
+		colors[i][0] = ofRandom(0, 255);
+		colors[i][1] = colors[i][0];
+		colors[i][2] = colors[i][0];
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	float time = ofGetElapsedTimef(); 
+	for (int i = 0; i < AMOUNT; i++) {
+		float x = ofGetWidth() * ofNoise(time * speedx[i]);
+		float y = ofGetHeight() * ofNoise(time * speedy[i]);
+		ofSetColor(colors[i][0], colors[i][1], colors[i][2]);
+		ofDrawCircle(x, y, 12);
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
